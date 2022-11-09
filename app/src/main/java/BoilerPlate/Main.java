0bab0fec0a99326bb29b4e724fbfe0e81beee68c
Martin Henriksson <mh225wd@student.lnu.se>
2022-11-09 14:13:05 +0100

    Remove encrypter

--- app/src/main/java/BoilerPlate/Main.java
+++ app/src/main/java/BoilerPlate/Main.java
@@ -1,14 +1,10 @@
 package BoilerPlate;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   private Console ui;
   private Scanner scan;
-  String[] arrFromFile;
 
   public Main() {
     this.scan = new Scanner(System.in, "UTF8");
@@ -17,41 +13,22 @@ public class Main {
 
   public void run() {
     TextReader exampleText = new TextReader("/src/main/java/files/example.txt");
-    this.arrFromFile = exampleText.readFromFile().split("\n");
-    
-    // getKey(40, "QMJ BPZ B XPJZ RZWJPAXQ LAD");
-    Encrypter encrypter = new Encrypter('z', 'a');
-    String message = "XEM CKSX MEET MEKBT Q MEET SXKSA SXKSA YV Q MEET SXKSA SEKBT SXKSA MEET".toLowerCase();
-    getKey(40, message, false, encrypter);
+
+    Boolean isRunning = true;
+    do {
+      // Main loop goes here.
+      System.out.println(exampleText.readFromFile());
+
+      int choice = this.ui.promptForInt("Enter a number: ");
+
+      if (choice == 0) {
+        isRunning = false; 
+      }
+    } while (isRunning);
 
 
-    System.out.println(encrypter.decrypt(16, message));
+
     // Close scanner when exit application
     scan.close();
-  }
-
-  private void getKey(int cycles ,String message, boolean showEachLine, Encrypter encrypter) {
-    for (int i = 0; i < cycles; i++) {
-      if (showEachLine) {
-        System.out.println("Key " + i + " contains " + encrypter.decrypt(i, message));
-      } else {
-        String[] sentence = encrypter.decrypt(i, message).split(" ");
-
-        for (String s : sentence) {
-          if (isValidWord(s)) {
-            System.out.println("Key " + i + " contains " + s);
-          }
-        }
-      }
-    }
-  }
-
-  private boolean isValidWord(String word) {
-    for (String s : arrFromFile) {
-      if (s.equals(word)) {
-        return true;
-      }
-    }
-    return false;
   }
 }
